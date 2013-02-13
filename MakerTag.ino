08591000b2fe481eaaca7cf9a1bb46b946dac05c
Ben Cannon <cannon.benjamind@gmail.com>
2013-02-13 15:52:52 -0800

    Adds .ino file

---
+++ MakerTag.ino
@@ -0,0 +1,179 @@
+/*
+ * Laser Tag gun
+ * An IR LED must be connecte    d to Arduino PWM pin 3.
+ * Pew Pew
+ */
+
+#include <IRremote.h>
+
+// Declare some IR shit
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+IRsend irsend;
+int RECV_PIN = 6;     // IR Recieve (PD6)
+int IR_FIRE = 3;      // IR LED for transmit(PD3)
+
+// Declare pin numbers
+int FIRE_GUN_PIN = 2;        // Trigger (PD2)
+int RESET_GUN_PIN = 5;   // When Dead Reset for more lives (PD5)
+int RELOAD_GUN_PIN = 4;  // PD4
+int HIT_LED0 = 9;        // Led0 - led4 indicates how many lives are left (PB1)
+int HIT_LED1 = 10;       // (PB2)
+int HIT_LED2 = 11;       // (PB3)
+int HIT_LED3 = 12;       // (PB4)
+int HIT_LED4 = 13;       // (PB5)
+int TEAM_A_PIN    = 0;       // Select Team A (PC0)
+int TEAM_B_PIN    = 1;       // Select Team B (PC1)
+int TEAM_FREE_PIN = 2;       // Selct Free-For-All(PC2)
+
+
+// Sound Effect Pins (when high, the sound plays in the gun)
+int SHOOT_SFX = 7;      // (PD7)
+int EMPT_REL_SFX = 8;   // (PB0) 
+
+
+// Team Identifiers and config values
+int freeForAll = 0xb25;  // Default value for free for all
+int teamA = 0xb4A;       // Value for team A
+int teamB = 0xDB;        // Value for B
+int triggerDelay = 250;  // ms that the gun will be inactve after trigger press
+
+// Variables for gun functionality
+volatile int hit;
+volatile int ammo;
+int currentTeam;      // The current gun ID
+int cureentEnemy;     // People trying to kill you
+
+
+/********************
+  Set up the gun. This routine runs only once
+*********************/
+void setup()
+{ 
+  // Initialize variables
+  hit = 0;    // No Hits yet...
+  ammo = 10;  // Start off with ten rounds
+  
+  // Set up the pins
+  // Inputs
+  pinMode(FIRE_GUN_PIN,INPUT_PULLUP);
+  pinMode(RESET_GUN_PIN,INPUT_PULLUP);
+  pinMode(RELOAD_GUN_PIN, INPUT_PULLUP);
+  pinMode(TEAM_A, INPUT_PULLUP);
+  pinMode(TEAM_B, INPUT_PULLUP);
+  pinMode(TEAM_FREE, INPUT_PULLUP);
+
+  // Output
+  pinMode(HIT_LED0, OUTPUT);
+  pinMode(HIT_LED1, OUTPUT);
+  pinMode(HIT_LED2, OUTPUT);
+  pinMode(HIT_LED3, OUTPUT);
+  pinMode(HIT_LED4, OUTPUT); 
+  
+  delay(10);  // Not completely required but gives the
+              // chances to propegate
+              
+  // Team selection
+  // Check the 3 position switch to check what team mode and or team.
+  if(digitalRead(TEAM_A_PIN) == LOW){ // You're on team A
+    currentTeam = teamA;
+    currentEnemy = teamB;
+  }
+  else if(digitalRead(TEAM_B_PIN == LOW)){
+    currentTeam = teamB;
+    currentEnemy = teamA;
+  }
+  else if(digitalRead(TEAM_FREE_PIN) == LOW){
+    currentTeam = freeForAll;
+    currentEnemy = freeForAll;
+  }
+  
+  // Attach some interrupts
+  attachInterrupt(FIRE_GUN_PIN, shoot, LOW);  //
+  attachInterrupt(RELOAD_GUN_PIN, reload, LOW); 
+  
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {// If there is some decoded values available 
+      if(results.value == currentEnemy){ // If it matches our val, increment
+        hit++;
+       if (hit==1)
+          digitalWrite(HIT_LED0, HIGH);
+        else if(hit==2)
+          digitalWrite(HIT_LED1, HIGH);
+        else if(hit==3) {
+          digitalWrite(HIT_LED3, HIGH);
+        else if(hit == 4)
+          digitalWrite(HIT_LED4, HIGH);
+        else if(hit == 5){  // You are dead.
+          digitalWrite(HIT_LED, HIGH);
+          delay(200);
+          while(hit==3 && digitalRead(5)==HIGH){
+            digitalWrite(7,LOW);
+            digitalWrite(8,LOW);
+            digitalWrite(9,LOW);
+            delay(80);
+            digitalWrite(7,HIGH);
+            digitalWrite(8,HIGH);
+            digitalWrite(9,HIGH);
+            delay(80);
+            digitalWrite(7,LOW);
+            digitalWrite(8,LOW);
+            digitalWrite(9,LOW);
+          }
+          hit=0;
+        }  
+      }     
+      irrecv.resume(); // Receive the next value
+    }
+  }
+
+
+
+
+/*
+  This subroutine will be called while the interupt is called
+  in this subroutine a hex value will be sent via IR 
+*/
+void shoot() {
+  cli();  // Disable interrupts to be a little atomic
+  
+  if ( ammo > 0 ){// If there is still ammo
+    // Decrement ammo
+    ammo--;
+    irsend.sendRC5(currentTeam, 12); // Send Hex value via IR
+    digitalWrite(SHOOT_SFX, HIGH);    // Make pew noise
+    
+    // While the trigger is pressed do nothing
+    while (digitalRead(FIRE_GUN_PIN) == LOW){;}
+    
+    // Wait for debounce and to limit shoots
+    delay(triggerDelay);
+    digitalWrite(SHOOT_SFX,LOW);
+  }
+  else{// Else there is no ammo left. Do not shoot and make empty sound
+    // While trigger is pressed do nothing
+    while(digitalRead(FIRE_GUN_PIN) == LOW){;} 
+    delay(triggerDelay);  // Delay for a bit. No Rapid fire!!
+  }
+  // On the way out... 
+  irrecv.enableIRIn(); // Enable the Receive in... I don't know why
+  sei();               // Enable global interrupts 
+}
+
+/*
+  Reload the gun by sliding the hammer back
+*/
+void reload(){
+  cli();  // Disable interrupts
+  while(digitalRead(RELOAD_GUN_PIN) == LOW){;} // Do nothing while the hammer is back
+  digitalWrite(RELOAD_SFX, HIGH);  // Make the reload noise
+  delay(triggerDelay);             // Delay for a bit
+  digitalWrite(RELOAD_SFX, LOW);   
+  ammo = 10;                       // Reload the gun
+  irrecv.enableIRIn(); // Enable the Receive in... I don't know why
+  sei();               // Enable global interrupts 
+}
+ 
